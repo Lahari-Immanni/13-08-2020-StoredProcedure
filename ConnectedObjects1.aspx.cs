8c260ef8294e3bfebd8b2ac655e5d66cb1dc6985
lahari <lahari.immanni@zensar.com>
2020-08-14 02:15:56 -0700

    stored procedure

---
+++ ConnectedObjects1.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SqlAssign
+{
+    public partial class ConnectedObjects1 : System.Web.UI.Page
+    {
+        SqlConnection conn = new SqlConnection("Data Source=LAPTOP-NSAR4TPI;Initial Catalog=AssignDotNet;Integrated Security=True");
+        SqlCommand cmd;
+        SqlDataReader dr;
+        DataTable dt;
+        public void ShowGrid()
+        {
+            conn.Open();
+            cmd = new SqlCommand("select * from EmployeeTb1", conn);
+            dr = cmd.ExecuteReader();
+            dt = new DataTable();
+            dt.Load(dr);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+            dr.Close();
+            conn.Close();
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ShowGrid();
+            }
+
+        }
+
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            cmd = new SqlCommand("InsertEmp", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@empname", SqlDbType.VarChar, 20).Value = TxtEmpName.Text;
+            cmd.Parameters.Add("@empsal", SqlDbType.Float).Value = Convert.ToSingle(TxtEmpSal.Text);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            ShowGrid();
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            cmd = new SqlCommand("sp_UpdateEmp", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@empid", SqlDbType.Int).Value = Convert.ToInt32(TxtEmpId.Text);
+            cmd.Parameters.Add("@empname", SqlDbType.VarChar, 20).Value = TxtEmpName.Text;
+            cmd.Parameters.Add("@empsal", SqlDbType.Float).Value = Convert.ToSingle(TxtEmpSal.Text);
+            int x = cmd.ExecuteNonQuery();
+            conn.Close();
+            ShowGrid();
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            cmd = new SqlCommand("delete from EmployeeTb1 where empid=" + TxtEmpId.Text + "", conn);
+            int x = cmd.ExecuteNonQuery();
+            conn.Close();
+            ShowGrid();
+
+        }
+    }
+}
